8176caea368bbeffd86ee70bdd342c310063a59d
jprakash0205 <jprakash.ora@gmail.com>
2015-09-30 00:35:07 -0700

    commit logger

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'temp123456789#'
 
 OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
@@ -15,3 +14,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+#Mail Server Settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+#Administators
+ADMINS = ['you@example.com','jayaprakash.nallathambi@one.verizon.com']
